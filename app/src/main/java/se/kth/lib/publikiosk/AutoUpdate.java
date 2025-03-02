6b9d59a43acc1445eb3f797277526385f9ecae74
Thomas Lind <tholind@kth.se>
2025-03-02 15:49:33 +0100

    yml

--- app/src/main/java/se/kth/lib/publikiosk/AutoUpdate.java
+++ app/src/main/java/se/kth/lib/publikiosk/AutoUpdate.java
@@ -1,83 +1,97 @@
 package se.kth.lib.publikiosk;
 
-import android.app.admin.DevicePolicyManager;
+package com.example.autoupdate;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Environment;
+import android.util.Log;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
 
-public class AutoUpdate extends AsyncTask<Void, Void, File> {
+import android.app.admin.DevicePolicyManager;
+
+public class AutoUpdate {
+
     private Context context;
+    private DevicePolicyManager dpm;
     private ComponentName admin;
+    private String githubDownloadUrl = "https://api.github.com/repos/kth-bibliotekete/publikiosk/actions/artifacts"; // <-- Ändra till ditt repo
+    private String apkName = "app-debug.apk"; // Standard APK namn
 
-    public AutoUpdate(Context context, ComponentName admin) {
+    public AutoUpdate(Context context) {
         this.context = context;
-        this.admin = admin;
+        dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        admin = new ComponentName(context, MyDeviceAdminReceiver.class);
     }
 
-    @Override
-    protected File doInBackground(Void... voids) {
+    public void checkForUpdate() {
         try {
-            // Hämta senaste GitHub Release
-            URL url = new URL("https://api.github.com/repos/kthbiblioteket/repo/releases/latest");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            InputStream is = conn.getInputStream();
-            Scanner scanner = new Scanner(is);
-            String response = scanner.useDelimiter("\\A").next();
-
-            JSONObject json = new JSONObject(response);
-            JSONArray assets = json.getJSONArray("assets");
-            String downloadUrl = assets.getJSONObject(0).getString("browser_download_url");
-
-            // Ladda ner APK
-            File apkFile = new File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "app-latest.apk");
-            downloadFile(downloadUrl, apkFile);
-
-            return apkFile;
-
+            URL url = new URL(githubDownloadUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            if (connection.getResponseCode() == 200) {
+                Log.d("AutoUpdate", "Ny version hittades!");
+                downloadAPK();
+            } else {
+                Log.d("AutoUpdate", "Ingen ny version");
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
         }
     }
 
-    @Override
-    protected void onPostExecute(File apkFile) {
-        if (apkFile != null) {
-            try {
-                DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-                Uri apkUri = Uri.fromFile(apkFile);
-                dpm.installPackage(admin, apkUri, DevicePolicyManager.INSTALL_ALLOW_DOWNGRADE);
-            } catch (Exception e) {
-                e.printStackTrace();
+    private void downloadAPK() {
+        String downloadUrl = "https://github.com/kth-biblioteket/publikiosk/releases/latest/download/" + apkName;
+
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(downloadUrl));
+        request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, apkName);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+        DownloadManager manager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        long downloadId = manager.enqueue(request);
+
+        context.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                installAPK(downloadId);
+            }
+        }, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    }
+
+    private void installAPK(long downloadId) {
+        DownloadManager manager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = manager.getUriForDownloadedFile(downloadId);
+
+        if (uri != null) {
+            File file = new File(context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), apkName);
+            Uri apkUri = FileProvider.getUriForFile(context, context.getPackageName() + ".fileprovider", file);
+
+            if (dpm.isDeviceOwnerApp(context.getPackageName())) {
+                dpm.setPermissionGrantState(
+                        admin,
+                        context.getPackageName(),
+                        android.Manifest.permission.REQUEST_INSTALL_PACKAGES,
+                        DevicePolicyManager.PERMISSION_GRANT_STATE_GRANTED
+                );
+
+                Intent installIntent = new Intent(Intent.ACTION_INSTALL_PACKAGE);
+                installIntent.setData(apkUri);
+                installIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(installIntent);
+                Log.d("AutoUpdate", "Installerar APK...");
+            } else {
+                Log.e("AutoUpdate", "Appen är inte Device Owner!");
             }
         }
-    }
-
-    private void downloadFile(String downloadUrl, File file) throws Exception {
-        URL url = new URL(downloadUrl);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        InputStream is = conn.getInputStream();
-        FileOutputStream fos = new FileOutputStream(file);
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = is.read(buffer)) > 0) {
-            fos.write(buffer, 0, length);
-        }
-        fos.close();
-        is.close();
     }
 }
